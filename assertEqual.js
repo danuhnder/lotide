804e7a90118294fac85090fbf429f359cafff277
Daniel Robertson <danieljrobertson@gmail.com>
2020-10-26 18:07:01 +0000

    initial commit of a function to assert if two values are equal

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Dan", "best looking dude");
+assertEqual(true, true);
+assertEqual(true, false);
+assertEqual('abcd', 'abcd');
+assertEqual('abcd', 'ABCD');
+assertEqual(22, 22);
+assertEqual(22.0, 22.00);
