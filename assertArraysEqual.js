124676c7232c945234213f9a264e98f2a973ae99
Daniel Robertson <danieljrobertson@gmail.com>
2020-10-27 16:43:25 +0000

    initial commit of a function to assert if two arrays are equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,6 +2,7 @@ const assertArraysEqual = function(array1, array2) {
   let failCondition = 0;
   if (array1.length !== array2.length) {
     console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+    failCondition = 1;
   } else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i] && failCondition === 0) {
