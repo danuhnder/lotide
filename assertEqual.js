424fd7eae031d1756de382e6e14b2451965d3b8b
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-02 17:43:49 +0000

    refactoring to split tests into separate files

--- assertEqual.js
+++ assertEqual.js
@@ -6,12 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Dan", "best looking dude");
-assertEqual(true, true);
-assertEqual(true, false);
-assertEqual('abcd', 'abcd');
-assertEqual('abcd', 'ABCD');
-assertEqual(22, 22);
-assertEqual(22.0, 22.00);
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-console.log(head([3,5,7,9]));
-console.log(head(['leprechaun', 'bunny', 'tiger']));
-console.log(head([3]));
-console.log(head([]));
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5, 8, 9]), 5);
-assertEqual(head([true, false, false]), false);
+module.exports = head; 
