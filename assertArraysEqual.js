8d4f8ea8fa7aa04422ed9ee965fce50961510c0e
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-02 17:56:59 +0000

    refactoring of assertArraysEqual.js and split off tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,40 +1,4 @@
-// const assertArraysEqual = function(array1, array2) {
-//   let failCondition = 0;
-//   if (array1.length !== array2.length) {
-//     console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-//     failCondition = 1;
-//   } else {
-//     for (let i = 0; i < array1.length; i++) {
-//       if (array1[i] !== array2[i] && failCondition === 0) {
-//         console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-//         failCondition = 1;
-//       }
-//     }  
-//   }  
-//   if (failCondition === 0) {
-//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-//   } 
-// };
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-    
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -45,10 +9,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+module.exports = assertArraysEqual;
 
-
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
