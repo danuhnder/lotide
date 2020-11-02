69bb89289c2b5fc7ff477d086de75bbd53519421
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-02 17:53:03 +0000

    refactoring of eqArray.js and split off tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
@@ -19,8 +11,5 @@ const eqArrays = function (array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+module.exports = eqArrays;
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
