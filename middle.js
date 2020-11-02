56098bddd3f9a8a56f5467cf1e72c062cf1ef24b
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-02 18:01:03 +0000

    refactoring of middle.js and split off tests

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   if (Array.isArray(array)) {
     const arrayLength = array.length;
@@ -36,7 +15,6 @@ const middle = function(array) {
     return outputArray;
   }
 };
-// TEST CODE
-assertArraysEqual(middle([0, 1, 2, 3, 4, 5, 6, 7]), [3, 4]);
-assertArraysEqual(middle([0, 1, 2, 3, 4, 5, 6, 7, 8]), [4]);
-assertArraysEqual(middle([0, 1]), []);
+
+module.exports = middle;
+
